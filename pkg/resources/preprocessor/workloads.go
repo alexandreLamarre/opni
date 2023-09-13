e1c6637187b52219a3175e71dcbc5835d31c3cc1
Joao Henri <joao.henri@suse.com>
2023-09-13 16:05:35 -0300

    Remove TraceCollectorConfig
    Signed-off-by: Joao Henri <joao.henri@suse.com>

--- pkg/resources/preprocessor/workloads.go
+++ pkg/resources/preprocessor/workloads.go
@@ -76,8 +76,6 @@ processors:
       statements:
       - set(attributes["log_type"], "controlplane") where attributes["k8s.pod.labels.tier"] == "control-plane"
       - set(attributes["kubernetes_component"], attributes["k8s.pod.labels.component"]) where attributes["k8s.pod.labels.tier"] == "control-plane"
-  no_op:
-    name: no_op
 
 exporters:
   opensearch/logs:
@@ -111,7 +109,6 @@ service:
       exporters: ["opensearch/logs"]
     traces:
       receivers: ["otlp"]
-      processors: ["no_op"]
       exporters: ["opensearch/traces"]
 `))
 )
