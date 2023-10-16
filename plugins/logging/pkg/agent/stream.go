c09a2075b6b49d9401cd94e4e0192e8da7076230
Janelle Law <janelle.law@suse.com>
2023-10-16 16:39:18 +0000

    configure plugin loggers with agent id

--- plugins/logging/pkg/agent/stream.go
+++ plugins/logging/pkg/agent/stream.go
@@ -2,10 +2,13 @@ package agent
 
 import (
 	capabilityv1 "github.com/rancher/opni/pkg/apis/capability/v1"
+	controlv1 "github.com/rancher/opni/pkg/apis/control/v1"
+	"github.com/rancher/opni/pkg/logger"
 	streamext "github.com/rancher/opni/pkg/plugins/apis/apiextensions/stream"
 	"github.com/rancher/opni/plugins/logging/apis/node"
 	collogspb "go.opentelemetry.io/proto/otlp/collector/logs/v1"
 	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (p *Plugin) StreamServers() []streamext.Server {
@@ -22,7 +25,17 @@ func (p *Plugin) StreamServers() []streamext.Server {
 }
 
 func (p *Plugin) UseStreamClient(cc grpc.ClientConnInterface) {
+	p.configureLoggers(cc)
 	nodeClient := node.NewNodeLoggingCapabilityClient(cc)
 	p.node.SetClient(nodeClient)
 	p.otelForwarder.SetClient(cc)
+}
+
+func (p *Plugin) configureLoggers(cc grpc.ClientConnInterface) {
+	identityClient := controlv1.NewIdentityClient(cc)
+	id, err := identityClient.Whoami(p.ctx, &emptypb.Empty{})
+	if err != nil {
+		p.logger.Error("error fetching node id", logger.Err(err))
+	}
+	logger.SetPluginWriter(id.GetId())
 }
