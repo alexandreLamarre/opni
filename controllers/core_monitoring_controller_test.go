38ead78b297d163a8c8ca66d1b609fa73cee1546
Joao Henri <joao.henri@suse.com>
2023-10-23 16:35:53 -0300

    remove grafana v1alpha1
    Signed-off-by: Joao Henri <joao.henri@suse.com>

--- controllers/core_monitoring_controller_test.go
+++ controllers/core_monitoring_controller_test.go
@@ -3,7 +3,6 @@ package controllers_test
 import (
 	"context"
 	"fmt"
-	grafanav1alpha1 "github.com/grafana-operator/grafana-operator/v4/api/integreatly/v1alpha1"
 	"os"
 	"time"
 
@@ -240,13 +239,13 @@ var _ = Describe("Monitoring Controller", Ordered, Label("controller", "slow"), 
 				Enabled:  lo.ToPtr(true),
 				Hostname: lo.ToPtr("x"),
 			}))
-			Expect(target.Spec.Grafana.GrafanaSpec).To(Equal(grafanav1alpha1.GrafanaSpec{
-				Config: grafanav1alpha1.GrafanaConfig{
-					AuthGenericOauth: &grafanav1alpha1.GrafanaConfigAuthGenericOauth{
-						Enabled: lo.ToPtr(true),
-					},
-				},
-			}))
+			// Expect(target.Spec.Grafana.GrafanaSpec).To(Equal(grafanav1alpha1.GrafanaSpec{
+			// 	Config: grafanav1alpha1.GrafanaConfig{
+			// 		AuthGenericOauth: &grafanav1alpha1.GrafanaConfigAuthGenericOauth{
+			// 			Enabled: lo.ToPtr(true),
+			// 		},
+			// 	},
+			// }))
 			Expect(k8sClient.Delete(context.Background(), target)).To(Succeed())
 			Eventually(Object(target)).ShouldNot(Exist())
 		})
