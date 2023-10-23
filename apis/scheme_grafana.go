22c299aa8b0b26849552f67896d47e2d223ccdf5
Joao Henri <joao.henri@suse.com>
2023-10-23 14:03:34 -0300

    Vendor v1beta1 grafana CRDs
    Signed-off-by: Joao Henri <joao.henri@suse.com>

--- apis/scheme_grafana.go
+++ apis/scheme_grafana.go
@@ -3,11 +3,9 @@
 package apis
 
 import (
-	opnigrafanav1alpha1 "github.com/rancher/opni/apis/grafana/v1alpha1"
 	opnigrafanav1beta1 "github.com/rancher/opni/apis/grafana/v1beta1"
 )
 
 func init() {
-	addSchemeBuilders(opnigrafanav1alpha1.AddToScheme)
 	addSchemeBuilders(opnigrafanav1beta1.AddToScheme)
 }
